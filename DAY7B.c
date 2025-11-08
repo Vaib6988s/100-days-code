ad28e3be383f36ec4e87f71ad180585e018fb01d
Vaibhav Srivastav <VAIBHAV.22967@stu.upes.ac.in>
2025-11-08 17:13:32 +0530

    DAY7B.c

--- DAY7B.c
+++ DAY7B.c
@@ -7,7 +7,7 @@ int main() {
     printf("Enter a character: ");
     scanf("%c", &ch);
 
-    // Check for vowel or consonant (assuming input is a letter)
+    
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
         ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
         printf("%c is a vowel.\n", ch);
@@ -16,4 +16,5 @@ int main() {
     }
 
     return 0;
+
 }
