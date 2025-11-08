33362869b9e6115864365474142ef879e31b3a6e
Vaibhav Srivastav <VAIBHAV.22967@stu.upes.ac.in>
2025-11-08 17:12:09 +0530

    DAY4.c

--- DAY4.c
+++ DAY4.c
@@ -1,15 +1,14 @@
 //Write a program to swap two numbers without using a third variable.
 #include <stdio.h>
-int main()
-{
+
+int main() {
     int a, b;
-    printf("Enter your first number a\n:");
-    scanf("%d", &a);
-    printf("Enter your second number b\n:");
-    scanf("%d", &b);
-     a = a+b;
-     a = a-b;
-     printf("After swapping\n");
-     printf("First number is %d and second number is %d", b, a);
-     return 0;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
 }
