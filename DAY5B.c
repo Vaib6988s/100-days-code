45cbc3ba0beb99013412a7bbd6259aa646877abc
Vaibhav Srivastav <VAIBHAV.22967@stu.upes.ac.in>
2025-11-08 17:12:55 +0530

    DAY5B.c

--- DAY5B.c
+++ DAY5B.c
@@ -6,9 +6,10 @@ int main()
     printf("Enter time S\n:");
     scanf("%d", &S);
     H=S/3600;
-    M=S/60;
+    M=H%60;
     S=S%60;
 printf("Time is %d:Hours, %d:minutes, %d:seconds", H, M, S );
     return 0;
+
 
 }
